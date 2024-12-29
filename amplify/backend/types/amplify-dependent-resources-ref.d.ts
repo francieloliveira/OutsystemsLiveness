032a0dd3f1f733e3c2c9a02b68f5d4d9531bfd24
Franciel Oliveira <franciel.technix@gmail.com>
2024-12-29 00:42:38 -0300

    alterações na estrutura

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "livenessautonomia074880fa": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
