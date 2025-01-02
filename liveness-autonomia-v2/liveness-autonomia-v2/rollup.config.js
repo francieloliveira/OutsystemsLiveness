447a47f279c7edfa50edf29c9cfeebb774fe4f82
Franciel Oliveira <franciel.technix@gmail.com>
2025-01-02 16:21:53 -0300

    first LivenessBundle.js criado

--- liveness-autonomia-v2/liveness-autonomia-v2/rollup.config.js
+++ liveness-autonomia-v2/liveness-autonomia-v2/rollup.config.js
@@ -5,26 +5,29 @@ import json from '@rollup/plugin-json';
 import commonjs from '@rollup/plugin-commonjs';
 
 export default {
-  input: 'src/Components/LivenessQuickStartReact.js',
+  input: 'src/Components/LivenessQuickStartReact.js', // Arquivo de entrada
   output: {
-    dir: 'dist', // Gera múltiplos arquivos no diretório 'dist'
-    format: 'es', // Formato ES modules
-    sourcemap: true, // Mapa de fontes para debug
+    file: 'LivenessBundle.js',  // Arquivo único de saída
+    format: 'iife',  // Formato IIFE
+    name: 'LivenessBundle',  // Nome global da variável
+    sourcemap: true,  // Geração de sourcemap
+    inlineDynamicImports: true, // Força a inclusão de importações dinâmicas no arquivo único
   },
   plugins: [
+    babel({
+      exclude: 'node_modules/**', // Exclui node_modules do Babel
+      presets: [
+        '@babel/preset-env', // Para transformar JS moderno
+        '@babel/preset-react' // Para transformar JSX
+      ],
+      babelHelpers: 'bundled', // Configura os helpers do Babel
+    }),
     nodeResolve({
       preferBuiltins: true, // Prioriza módulos internos do Node.js
     }),
-    commonjs({
-      include: /node_modules/, // Garante que módulos CommonJS sejam convertidos corretamente
-    }),
-    babel({
-      exclude: 'node_modules/**', // Exclui node_modules do Babel
-      presets: ['@babel/preset-env', '@babel/preset-react'], // Suporte para JS moderno e JSX
-      babelHelpers: 'bundled', // Babel helpers para evitar importação de código extra
-    }),
-    json(), // Suporte para importar arquivos JSON
-    postcss(), // Suporte para processar CSS
+    commonjs(), // Adiciona suporte para módulos CommonJS
+    json(), // Adiciona suporte para importar arquivos JSON
+    postcss(), // Adiciona suporte para processar CSS
   ],
   context: 'window', // Define o contexto global para evitar problemas com `this`
 };
